e399e2243bd21565c0b3ee25f7ac12cdf9fb9cfc
LinnKlaas <linn.klaas@hfg-gmuend.de>
2020-07-27 14:54:43 +0200

    styling

--- src/components/ControlsComponent.vue
+++ src/components/ControlsComponent.vue
@@ -2,19 +2,24 @@
   <div id="controlscontainer">
         <strong>Connected: {{connected}}</strong><br><br><br>
         <div class="half">
-            <h4>Presets</h4>
 
     <b-button v-on:click="moveCamLeft()" :disabled="!connected || !amIActive"><b-icon icon="chevron-left" variant="success"></b-icon> links</b-button>
 
-      <svg width="8em" height="3em" viewBox="0 0 16 16" class="bi bi-camera-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <svg width="12em" height="3em" viewBox="0 0 16 16" class="bi bi-camera-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
         <path fill-rule="evenodd" d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
       </svg>
 
     <b-button v-on:click="moveCamRight()" :disabled="!connected || !amIActive">rechts <b-icon icon="chevron-right" variant="success"></b-icon></b-button>
     <br><br>
-    <b-button v-on:click="moveSliderLeft()" :disabled="!connected || !amIActive"><b-icon icon="chevron-left" variant="success"></b-icon> links</b-button>
-    <b-button v-on:click="moveSliderRight()" :disabled="!connected || !amIActive">rechts <b-icon icon="chevron-right" variant="success"></b-icon></b-button>
+    <b-button v-on:click="moveSliderLeft()" :disabled="!connected || buttonState === 'links' || !amIActive"><b-icon icon="chevron-left" variant="success"></b-icon> links</b-button>
+ 
+      <div class="slider-visualization">
+        <div class="slider-car"></div>
+        <div class="slider-base"></div>
+      </div>
+
+    <b-button v-on:click="moveSliderRight()" :disabled="!connected || buttonState === 'rechts' || !amIActive">rechts <b-icon icon="chevron-right" variant="success"></b-icon></b-button>
   </div>
   <div class="half">
             <h4>Current Waiting Queue {{this.currentTimer}}</h4>
@@ -34,19 +39,21 @@ export default {
             //CHANGEME: die Namen der Nachrichten die ihr mit emit() verschickt müssen mit dem Backend matchen
             moveCamLeft: function () {
                 console.log("Moving cam to the left");
-                this.$socket.emit('moveCamLeft');
+                this.$socket.emit('controlCam', -180);
             },
             moveCamRight: function () {
                 console.log("Moving cam to the right");
-                this.$socket.emit('moveCamRight');
+                this.$socket.emit('controlCam', 180);
             },
             moveSliderLeft: function () {
                 console.log("Moving slider to the left");
-                this.$socket.emit('moveSliderLeft');
+                this.$socket.emit('controlSlider', -200);
+                <div class="slider-car" style = "right: 2px"></div>
             },
             moveSliderRight: function () {
                 console.log("Moving slider to the right");
-                this.$socket.emit('moveSliderRight');
+                this.$socket.emit('controlSlider', 200);
+                <div class="slider-car" style = "right: -2px"></div>
             }
         },
 
@@ -76,6 +83,18 @@ export default {
             },
             client_name: function(data) {
                 this.clientName = data;
+            },
+            serialresponse: function(data) {
+                if (data === "rechts") {
+                    this.buttonState = "rechts"
+                } 
+                else if (data === "links") {
+                    this.buttonState = "links"
+                }
+                else if (data === "alles ok") {
+                    this.buttonState = ""
+                }
+
             }
         },
         data: function () {
@@ -85,6 +104,7 @@ export default {
                 ownId: "undefined",
                 clientName: "undefined",
                 currentTimer: 0,
+                buttonState: "",
             }
         },
         computed: {
@@ -103,4 +123,27 @@ export default {
         width: 50%;
         vertical-align: top;
     }
+  .buttons {
+        display: inline-block;
+  }
+  .slider-visualization {
+        display: inline-block;
+        margin-left: 20px;
+        margin-right: 20px;
+  }
+  .slider-base {
+        padding: 2px;
+        border-left: 150px solid black;
+        position: relative;
+        bottom: -15px;
+  }
+  .slider-car {
+        height: 15px;
+        width: 25px;
+        background-color: #f9cb30;
+        position: relative;
+        float: right;
+        right: 4px;
+        bottom: 0px;
+  }
 </style>
