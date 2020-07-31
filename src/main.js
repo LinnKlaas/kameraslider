4b3ab81431a3d8900c5bffd55372595f8a786f0a
LinnKlaas <linn.klaas@hfg-gmuend.de>
2020-07-31 11:53:41 +0200

    updated main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,34 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
+import Vuex from 'vuex';
 import App from './App.vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-
+import VueSocketIO from 'vue-socket.io'
 Vue.config.productionTip = false
-
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'https://hal.hfg.design:10000/linnbot_control', //WICHTIG: Hier nach dem Slash den Namen eures Bots angeben (wie auf dem Pi festgelegt)
+  options: {
+    useConnectionNamespace: true
+  }
+}))
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+    currentQueue: [],
+    ownId: 'none',
+    ownName: 'noname',
+    clientName: 'undefined',
+    currentTimer: 0,
+  },
+  getters: {
+    amIActive: () => {
+      if (store.state.currentQueue.length === 0) return false;
+      return store.state.currentQueue[1][0].id === store.state.ownId;
+    },
+  },
+});
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
